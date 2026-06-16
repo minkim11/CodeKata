e6168d6b44c682a490d42df30408b92619733da7
minkim11 <alsgur4772@gmail.com>
2026-06-16 09:45:25 +0900

    Q58

--- src/level3/Q58.java
+++ src/level3/Q58.java
@@ -6,8 +6,8 @@ public class Q58 {
         int answer = 0;
 
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = 1; j < nums.length - 1; j++) {
-                for (int k = 2; k < nums.length; k++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
                     int sum = nums[i] + nums[j] + nums[k];
                     boolean check = true;
                     for (int l = 2; l <= Math.sqrt(sum); l++) {
