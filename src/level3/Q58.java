cd6b443f58c9c4304d85afded591b420e8ff0425
minkim11 <alsgur4772@gmail.com>
2026-06-16 09:43:50 +0900

    Q58

---
+++ src/level3/Q58.java
@@ -0,0 +1,28 @@
+package level3;
+
+// 소수만들기 https://school.programmers.co.kr/learn/courses/30/lessons/12977
+public class Q58 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = 1; j < nums.length - 1; j++) {
+                for (int k = 2; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    boolean check = true;
+                    for (int l = 2; l <= Math.sqrt(sum); l++) {
+                        if (sum % l == 0) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
